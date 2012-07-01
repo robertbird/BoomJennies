cbcc6e10070d101b240ea1226fce414f555ef357
Robert Bird <git@robertbird.co.uk>
2012-07-01 23:55:01 +0100

    Remaining pages and style tweaks

---
+++ Themes/Boomstrap/EmptyShapes.cs
@@ -0,0 +1,13 @@
+﻿public class EmptyShapes : IShapeTableProvider {
+
+        public void Discover(ShapeTableBuilder builder) {
+            builder.Describe("Parts_ProjectionPart")
+                .OnDisplaying(displaying => {
+                    var shape = displaying.Shape;
+                    if (shape.List.Items.Count == 0) {
+                        shape.Metadata.Alternates.Add("Parts_ProjectionPart__Empty");
+                    }
+                });
+        }
+    }
+
